73d73457a558b4db11cc210a3d04668be1b2b9e5
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-09-01 16:05:02 -0700

    -

--- Makefile
+++ Makefile
@@ -1,21 +1,14 @@
-.PHONY: all build clean
-OUT=bin
-all : clean build
-clean :
-	rm -rf bin
-build : $(wildcard *.c)
-	-clear
+.PHONY: all clean c_build rust_build
+OUT = bin
+SRC_C = $(wildcard *.c)
+EXE_C = $(patsubst %.c,$(OUT)/%,$(SRC_C))
+all : $(EXE_C) rust_build
+$(OUT)/% : %.c
 	mkdir -p $(OUT)
-	$(foreach i, \
-		$^, \
-		-clear -x && \
-		echo "<$(i)>" && \
-		gcc $(i) -o $(patsubst %.c, \
-			$(OUT)/%, \
-			$(i) \
-		) && $(patsubst %.c, \
-			./$(OUT)/%, \
-			$(i)\
-		) \
-		&& \
-	) echo
+	gcc $< -o $@
+	$@
+rust_build : dotconfig/*
+	cd dotconfig && \
+	cargo build
+clean :
+	rm -rf $(OUT)
--- pi.c
+++ pi.c
@@ -1,32 +1,26 @@
-#include <stdio.h>
 #include <math.h>
-#include <locale.h>
+#include <stdio.h>
 #define LEN(arr) sizeof(arr) / sizeof(*arr)
-int main()
-{
-	setlocale(LC_NUMERIC,"en_CA.UTF-8");
-	float PI = 3.14159265359;
-	void getPI(int A, int B)
-	{
-		float piApp = (float)A / (float)B;
-		float dif = PI - piApp;
-		printf("PI: %f (%d / %d)\n\t%06d\n", piApp, A, B, (int)(fabs(dif) * pow(10,7)));
-	};
-	int approxPI[][2] = {
-		{355, 113},		//	2
-		{3927, 1250},	//	72
-		{377, 120},		//	739
-		{223, 71},		//	7,477
-		{22, 7},		//	12,643
-		{339, 108},		//	27,039
-		{25, 8},		//	165,927
-		{256, 81},		//	189,011
-		{16, 5}			//	584,073
-	};
-	int i;
-	for (i = 0; i < LEN(approxPI); i++)
-	{
-		getPI(approxPI[i][0], approxPI[i][1]);
-	};
-	return 0;
+void main() {
+    float PI = 3.14159265359;
+    void getPI(int A, int B) {
+        float piApp = (float)A / (float)B;
+        float dif = PI - piApp;
+        printf("PI: %f (%d / %d)\n\t%06d\n", piApp, A, B, (int)(fabs(dif) * pow(10, 7)));
+    };
+    int approxPI[][2] = {
+        {355,  113 },
+        {3927, 1250},
+        {377,  120 },
+        {223,  71  },
+        {22,   7   },
+        {339,  108 },
+        {25,   8   },
+        {256,  81  },
+        {16,   5   }
+    };
+    int i;
+    for (i = 0; i < LEN(approxPI); i++) {
+        getPI(approxPI[i][0], approxPI[i][1]);
+    };
 }
