c25d93e44027fa5b45e5d162a7ef899a70484ac6
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-09-01 16:23:07 -0700

    -

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use walkdir::WalkDir;
-
 fn main() {
     for entry in WalkDir::new(".")
         .into_iter()
         .filter_map(|e| e.ok())
-        .filter(|e| e.path().extension().map_or(false, |ext| ext == "c"))
+        .filter(|e| {
+            e.path()
+                .extension()
+                .map_or(false, |ext| ext == "c")
+        })
     {
-        println!("{}", entry.path().display());
+        println!("{}", entry.path().display())
     }
 }
